6927be71b7959993d20595dbe05cfe66aaef390c
Nathaniel Wheeler <nathan@mailftp.com>
2020-10-14 22:18:38 -0600

    front page changed

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>ThemeCore</h1>
 	<p>
 		Visit the
-		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-		to learn how to build Svelte apps.
+		<a href="https://github.com/nathanielwheeler/themecore.app.git">repository</a>
+		to follow me building this app up!
 	</p>
 </main>
 
